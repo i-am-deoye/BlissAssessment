cf036b016f825d6f9230353ab1dbe49da237bbc8
Ayankoya Moses <mosestrendlr@gmail.com>
2022-08-01 10:47:37 +0100

    Delete .github directory

--- .github/test_app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild -workspace FingoApp.xcworkspace \
-            -scheme FingoApp \
-            -destination platform=iOS\ Simulator,name=iPhone\ 11 \
-            clean test | xcpretty
